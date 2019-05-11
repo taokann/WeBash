b85d8e0f2e5d99b4121180658327c5409ecd1776
colivier74 <colivier.dev@gmail.com>
2019-05-11 13:16:06 +0200

    master upgrade

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-/usr/bin/git -C /home/chevro/WeBash-testing/WeBash pull
-/usr/local/bin/docker-compose -f /home/chevro/WeBash-testing/WeBash/docker-compose.yml up --build -d
+/usr/bin/git -C /home/chevro/WeBash/WeBash pull
+/usr/local/bin/docker-compose -f /home/chevro/WeBash/WeBash/docker-compose.yml up --build -d
 
 #You can add a crontab for this script for update your server automatically (change the path with your's)
 #* * * * * /bin/sh /home/chevro/WeBash/start.sh
