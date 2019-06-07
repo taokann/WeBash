a3e0035bf0af75ef761d9b5c756064eae6fc0fd7
taokann.one <site@taokann.one>
2019-06-07 21:26:06 +0200

    Add license note in each source code file

--- res/scripts/echo.js
+++ res/scripts/echo.js
@@ -1,3 +1,15 @@
+/*
+ *
+ * File name: echo.js
+ * Description: echo command
+ * Authors: taokann.one and colivier74
+ * If you're a new WeBash contributor and worked on this file, please add you name here.
+ *
+ * This file is part of the WeBash project with is released under the terms of GNU General Public License V3.0.
+ * You should have recieved a copy of it along with this file, if not, please write to :
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth floor, Boston, MA 02110-1301 USA.
+ */
+
 exports.run = (query) => {
     return new Promise((resolve, reject) => {
         query.shift()
--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*
+ *
+ * File name: server.js
+ * Description: main node.js script file
+ * Authors: taokann.one and colivier74
+ * If you're a new WeBash contributor and worked on this file, please add you name here.
+ *
+ * This file is part of the WeBash project with is released under the terms of GNU General Public License V3.0.
+ * You should have recieved a copy of it along with this file, if not, please write to :
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth floor, Boston, MA 02110-1301 USA.
+ */
+
 var express = require("express")
 var headers = require("headersfromextensions")
 var fs = require("fs")
